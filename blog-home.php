4236cbf185cf10fad9f83ff8830ed2a8b314f499
wooju0512 <52991086+wooju0512@users.noreply.github.com>
2020-11-09 14:25:53 +0900

    Add files via upload

---
+++ blog-home.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Blog Home</title>
+
+    <!-- Favicon -->
+    <link href="https://fonts.googleapis.com/css2?family=Poor+Story&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        *{
+            font-family: 'Poor Story', cursive;
+        }
+        </style>
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+    <section class="banner-area banner-area2 blog-page text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><i><a href="./">음식이름</a></i></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br><br>
+    <!--================Blog Area =================-->
+    <section class="blog_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="blog_left_sidebar">
+                        <article class="row blog_item">
+                           <div class="col-md-3">
+                               <div class="blog_info text-right">
+                                    <div class="post_tag">
+                                        <a class="active" href="#">첫번째</a>
+                                    </div>
+                                    <ul class="blog_meta list">
+                                        
+                                    </ul>
+                                </div>
+                           </div>
+                            <div class="col-md-9">
+                                <div class="blog_post">
+                                    <img src="assets/images/blog/main-blog/m-blog-1.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="blog-details.html"><h4>Astronomy Binoculars A Great Alternative</h4></a>
+                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.</p>
+                                        <a href="blog-details.html" class="template-btn">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        
+                        <article class="row blog_item">
+                           <div class="col-md-3">
+                               <div class="blog_info text-right">
+                                    <div class="post_tag">
+                                        <a class="active" href="#">두번쨰</a>
+                                    </div>
+                                    <ul class="blog_meta list">
+                                    </ul>
+                                </div>
+                           </div>
+                            <div class="col-md-9">
+                                <div class="blog_post">
+                                    <img src="assets/images/blog/main-blog/m-blog-3.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="blog-details.html"><h4>The Glossary Of Telescopes</h4></a>
+                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.</p>
+                                        <a href="blog-details.html" class="template-btn">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <article class="row blog_item">
+                           <div class="col-md-3">
+                               <div class="blog_info text-right">
+                                    <div class="post_tag">
+                                    <a class="active" href="#">세번째</a>
+                                        
+                                    </div>
+                                    <ul class="blog_meta list">
+                                    </ul>
+                                </div>
+                           </div>
+                            <div class="col-md-9">
+                                <div class="blog_post">
+                                    <img src="assets/images/blog/main-blog/m-blog-4.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="blog-details.html"><h4>The Night Sky</h4></a>
+                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.</p>
+                                        <a href="blog-details.html" class="template-btn">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <article class="row blog_item">
+                           <div class="col-md-3">
+                               <div class="blog_info text-right">
+                                    <div class="post_tag">
+                                        <a class="active" href="#">네번째</a>
+                                    </div>
+                                    <ul class="blog_meta list">
+                                    </ul>
+                                </div>
+                           </div>
+                            <div class="col-md-9">
+                                <div class="blog_post">
+                                    <img src="assets/images/blog/main-blog/m-blog-5.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="blog-details.html"><h4>Telescopes 101</h4></a>
+                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.</p>
+                                        <a href="blog-details.html" class="template-btn">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <article class="row blog_item">
+                           <div class="col-md-3">
+                               <div class="blog_info text-right">
+                                    <div class="post_tag">
+                                        <a class="active" href="#">다섯번째</a>
+                                    </div>
+                                    <ul class="blog_meta list">
+                                    </ul>
+                                </div>
+                           </div>
+                            <div class="col-md-9">
+                                <div class="blog_post">
+                                    <img src="assets/images/blog/main-blog/m-blog-5.jpg" alt="">
+                                    <div class="blog_details">
+                                        <a href="blog-details.html"><h4>Telescopes 101</h4></a>
+                                        <p>MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.</p>
+                                        <a href="blog-details.html" class="template-btn">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Blog Area =================-->
+
+   
+
+
+    <!-- Javascript -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
